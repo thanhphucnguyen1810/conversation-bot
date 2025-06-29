36da475874796412a5c538be61d86e545baa41f5
thanhphucnguyen1810 <thanhphucnguyen54@gmail.com>
2025-06-29 16:23:42 +0700

    feat: Design App's Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Logo from './Shared/Logo'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <AppBar sx={{ bgColor: "transparent", position: "static", boxShadow: "none"}}>
+      <Toolbar sx={{ display: "flex"}}>
+        <Logo />
+      </Toolbar>
+    </AppBar>
   )
 }
 
---
+++ frontend/src/components/Shared/Logo.tsx
@@ -0,0 +1,31 @@
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Logo = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        marginRight: "auto",
+        alignItems: "center",
+        gap: "15px"
+      }}
+    >
+      <Link to={"/"}>
+        <img src="openai.png" alt="openai" width={"30px"} height={"30px"} className="image-inverted" />
+        <Typography
+          sx={{
+            display: { md: "block", sm: "none", xs: "none" },
+            mr: "auto",
+            fontWeight: "800",
+            textShadow: "2px 2px 20px #000"
+          }}
+        >
+          <span style={{ fontSize: "20px" }}>MERN</span>-GPT
+        </Typography>
+      </Link>
+    </div>
+  )
+};
+
+export default Logo;
